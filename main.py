21b15b29183195052658d7b3fb101c5e1fd6b85b
Anna Khizhniak <khizhnyakanna@gmail.com>
2026-08-03 20:53:16 +0200

    Moved film display logic to display module

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 # Panel создает рамку вокруг текста
 from rich.panel import Panel
 
+from rich.table import Table
+
 # Align отвечает за выравнивание текста и объектов
 from rich.align import Align
-
-# Table создает таблицы
-from rich.table import Table
 
 from ui_utils import  console, wait_for_enter
 from validators import get_integer, get_integer_in_range
@@ -16,6 +15,7 @@ from search_service import (
     get_categories,
     search_by_category,
 )
+from display import show_films_in_pages
 
 import os
 import mysql.connector
@@ -120,71 +120,6 @@ def main() -> None:
         else:
             console.print("\nНекорректный выбор. Попробуйте снова.")
             console.input("\nНажмите Enter для продолжения...")
-
-        #wait_for_enter()
-
-def show_films_in_pages(films):
-    """
-    Показывает фильмы порциями по 10 штук.
-    Пользователь может запросить следующую страницу
-    или вернуться в меню.
-    """
-
-    if not films:
-        console.print("\nНичего не найдено.")
-        wait_for_enter()
-        return
-
-    page_size = 10
-    start = 0
-
-    while start < len(films):
-
-        end = start + page_size
-
-        console.print(
-            f"\nПоказаны фильмы {start + 1}-{min(end, len(films))}"
-            f" из {len(films)}"
-        )
-
-        # for film in films[start:end]:
-            # print(
-            #     f"{film['title']} ({film['release_year']})"
-            # )
-        table = Table(title="Результаты поиска")
-
-        table.add_column("№", justify="right")
-        table.add_column("Название")
-        table.add_column("Год", justify="center")
-
-        for index, film in enumerate(films[start:end], start=start + 1):
-            table.add_row(
-    str(index),
-               film["title"],
-               str(film["release_year"]),
-            )
-
-        console.print(table)
-
-        # если показали последнюю страницу
-        if end >= len(films):
-            console.print("\n-=Конец списка.=-")
-            wait_for_enter()
-            break
-
-        while True:
-            choice = console.input(
-                "\nПоказать следующие 10 фильмов? (Enter — да, 0 — меню): "
-            ).strip()
-
-            if choice == "":
-                start = end
-                break
-
-            if choice == "0":
-                return
-
-            console.print("\nНекорректный выбор. Попробуйте снова.")
 
 
 def show_search_by_keyword():
