7106e1e0870df4085741b57e904b369d5beff021
Anna Khizhniak <khizhnyakanna@gmail.com>
2026-08-04 00:15:24 +0200

    Added docstrings to search service

--- search_service.py
+++ search_service.py
@@ -6,14 +6,33 @@ from mysql_connector import (
 
 
 def search_by_keyword(keyword):
+    """
+    Выполняет поиск фильмов по ключевому слову.
+
+    :param keyword: Ключевое слово для поиска.
+    :return: Список найденных фильмов.
+    """
     return get_films_by_keyword(keyword)
 
 
 def get_categories():
+    """
+    Возвращает список жанров с диапазоном доступных годов.
+
+    :return: Список жанров.
+    """
     return get_categories_with_years()
 
 
 def search_by_category(category_id, year_from, year_to):
+    """
+    Выполняет поиск фильмов по жанру и диапазону годов.
+
+    :param category_id: ID выбранного жанра.
+    :param year_from: Начальный год диапазона.
+    :param year_to: Конечный год диапазона.
+    :return: Список найденных фильмов.
+    """
     return get_films_by_category_id_and_year(
         category_id,
         year_from,
